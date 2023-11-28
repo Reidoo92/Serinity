231a43481d9a528db363c4a68e73cad73661288e
laid sereir <laid.sereir00@gmail.com>
2023-11-28 13:02:26 +0100

    Add

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,19 +3,6 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  # def create
-  #   @user = User.new(user_params)
-
-  #   if @user.save
-  #     @user.role = :doctor
-  #     # Redirection ou autre logique après une inscription réussie
-  #     redirect_to root_path, notice: 'Inscription réussie!'
-  #   else
-  #     # Afficher à nouveau le formulaire d'inscription en cas d'échec
-  #     render :new
-  #   end
-  # end
-
   private
 
   def user_params
